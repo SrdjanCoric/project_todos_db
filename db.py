bc981f1ab9c1abdbd3b9afa11982e1536fb24a11
Srdjan Coric <srdjancoric@MacBook-Pro.local>
2023-11-27 10:10:08 +0100

    find list

--- db.py
+++ db.py
@@ -11,12 +11,19 @@ class DatabasePersistence:
             result = cur.fetchall()
             lists = [dict(result) for result in result]
             for item in lists:
-                item.setdefault('todos', [])
+                todos = self._find_todos_for_list(item['id'])
+                item.setdefault('todos', todos)
             return lists
 
     def find_list(self, id):
         with self.connection.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
             cur.execute("SELECT * FROM lists WHERE id = %s", (id,))
             list = dict(cur.fetchone())
-            list.setdefault('todos', [])
+            todos = self._find_todos_for_list(id)
+            list.setdefault('todos', todos)
             return list
+
+    def _find_todos_for_list(self, list_id):
+        with self.connection.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+            cur.execute("SELECT * FROM todos WHERE list_id = %s", (list_id,))
+            return cur.fetchall()
